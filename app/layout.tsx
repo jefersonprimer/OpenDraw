c000696e4218e04e3415339752e29032be0c0fc6
jefersonprimer <jefersonpriimer@gmail.com>
2026-02-26 11:29:27 -0300

    refactor: change name of project and add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Primer - Whiteboard",
+  title: "OpenDraw Whiteboard",
   description: "create by Primer",
 };
 
